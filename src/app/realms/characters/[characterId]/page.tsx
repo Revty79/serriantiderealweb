098f19488a17b38e4658913806822b2d82ceef51
Revty79 <132003666+Revty79@users.noreply.github.com>
2026-09-18 21:34:34 -0600

    Fix local Character style imports

--- src/app/realms/characters/[characterId]/page.tsx
+++ src/app/realms/characters/[characterId]/page.tsx
@@ -1,4 +1,4 @@
-import "../../../../characters/character.css";
+import "../../../characters/character.css";
 import { LocalCharacterEditor } from "../local-character-editor";
 
 export default async function CharacterPage({params}:{params:Promise<{characterId:string}>}) {
